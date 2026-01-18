53361528a0a86dd2bcc0e2af360b508a81d62f13
yonatan biton <yuyu82114@gmail.com>
2026-01-18 19:59:52 +0200

    added testing python for ci

---
+++ esting.py
@@ -0,0 +1,20 @@
+aws_access_key_id = "AKIA1234567890123456"
+
+
+
+
+
+aws_secret_access_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+stripe_key = "sk_live_000000000000000000000000"
+
+stripe_key_api = "blablabla"
+
+
+github_token = "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"
+
+slack_user = "xoxp-1234567890-1234567890-1234567890-ab12cd34ef"
+slack_bot = "xoxb-123456789012-1234567890123-AbCdEfGhIjKlMnOpQrStUvWx"
+Google_API = "AIzaSyD-Fke9_3kL1mNopQrStUvWxYz12345678"
+Digital_Ocean_api = "dop_v1_1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef"
+
+fake_gitlab = "glpat-1234567890abcdefghij"
