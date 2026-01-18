a4ac4af14007caf6eb95a63aeffe918a5257c3d2
yonatan biton <yuyu82114@gmail.com>
2026-01-18 20:02:14 +0200

    added testing python for ci

--- esting.py
+++ esting.py
@@ -2,15 +2,13 @@ aws_access_key_id = "AKIA1234567890123456"
 
 
 
-
-
 aws_secret_access_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
 stripe_key = "sk_live_000000000000000000000000"
 
-stripe_key_api = "blablabla"
+ddstripe_key_api = "blablabla"
 
 
-github_token = "ghp_3eeWauSn0TkKUqt9Eq0D9cwGkiuKNZ0RO1X5"
+ddgithub_token = "ghp_3eeWauSn0TkKUqt9Eq0D9cwGkiuKNZ0RO1X5"
 
 slack_user = "xoxp-1234567890-1234567890-1234567890-ab12cd34ef"
 slack_bot = "xoxb-123456789012-1234567890123-AbCdEfGhIjKlMnOpQrStUvWx"
